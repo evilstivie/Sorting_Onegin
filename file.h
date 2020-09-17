d7aba47a343dab1e3950534bf164f2816bfa8b51
Sofya <53237546+evilstivie@users.noreply.github.com>
2020-09-18 01:19:28 +0300

    add procces file

--- file.h
+++ file.h
@@ -13,14 +13,16 @@ typedef struct {
 	size_t len;
 } str;
 
-int64_t get_file_size(FILE *file);
+size_t get_file_size(FILE *file);
 
 void get_file(FILE *file, char *raw_text, size_t text_size);
 
 void str_output(FILE *outp, str *text, int cnt);
 
-int calc_lines(const char *inp);
+size_t calc_lines(const char *inp);
 
 void procces_raw_text(char *raw_text, str *text);
+
+char* procces_file();
 
 #endif // FILE_H
