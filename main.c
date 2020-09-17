ae86b03f2e56e709ff176e730350bd342293ea20
Sofya <53237546+evilstivie@users.noreply.github.com>
2020-09-18 01:16:59 +0300

    restructing the project

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,86 +6,66 @@
 #include "sort.h"
 
 /**
-* \brief Free allocated memory
-*/
-
-void free_memory(str *file, size_t lines, char *raw_text) {
-    free(raw_text);
-    free(file);    
-}
-
-/**
 * \brief Print original text of poem
 *
 * \param outp file to output the text
-* \param tx array of chars 
+* \param tx array of chars
 */
 
 void print_original_text(FILE *outp, char *tx) {
 	while (*tx) {
-		fprintf(outp, "%s\n", tx);
-		while (*tx != '\0') {
+		fprintf(outp, "%c", *tx); // :NOTE: toooo slow
+        ++tx;
+		if (*tx == '\0') {
+		    fprintf(outp, "\n");
 			++tx;
 		}
-		++tx;
 	}
 }
 
 int main(const int argC, const char** argV) {
-	FILE *file = fopen("onegin.txt", "r");
-	if (ferror(file)) {
-        assert(!"Error while reading the file");
-    }
+	setlocale(LC_ALL, "ru_RU.CP1251");
 
-	size_t text_size = get_file_size(file);
+	char *raw_text = procces_file();
+	size_t cnt_lines = calc_lines(raw_text + 1);
 
-	char *raw_text = calloc(text_size + 2, sizeof(raw_text[0])); // +2'/0' on top and end
-	if (!raw_text) {
-		assert("Memory allocation failed");
-	}
-
-	raw_text[0] = '\0'; // '\0' on top
-	++raw_text; // move pointer to the start of the text
-
-	get_file(file, raw_text, text_size); 
-	
-	fclose(file);
-
-	int cnt_lines = calc_lines(raw_text);
-	
 	str *text = calloc(cnt_lines, sizeof(str));
 	if (!text) {
-		assert("Memory allocation failed");
-	}
-	procces_raw_text(raw_text, text);
+		fprintf(stderr, "%s\n", "Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+	procces_raw_text(raw_text + 1, text);
 
 	FILE *outp = fopen("output1.txt", "w");
 	if (ferror(outp)) {
-        assert("Error while reading the file");
+        fprintf(stderr, "%s\n", "Error while opening the file");
+        exit(EXIT_FAILURE);
     }
 
-	fprintf(outp,"%s", "------Left-Right Sorting------");
+	fprintf(outp,"%s \n", "------Left-Right Sorting------");
 
 	qsort(text, cnt_lines, sizeof(str), cmp_lr);
 	str_output(outp, text, cnt_lines);
 
-	fprintf(outp,"%s", "------Right-Left Sorting------");
+	fprintf(outp,"%s\n", "------Right-Left Sorting------");
 
 	qsort(text, cnt_lines, sizeof(text[0]), cmp_rl);
 	str_output(outp, text, cnt_lines);
 
-	fprintf(outp,"%s", "------Orgiginal Text------");
+	fprintf(outp,"%s\n", "------Original Text------");
 
-	print_original_text(outp, raw_text);
+	print_original_text(outp, raw_text + 1);
 
 	fclose(outp);
-	--raw_text;
-	free_memory(text, cnt_lines, raw_text);
+
+	free(raw_text);
+    free(text);
 }
 
 
 /*
--> getFileSize -> getFile -> FileToString -> getLines(\n) -> 
+-> getFileSize -> getFile -> FileToString -> getLines(\n) ->
 -> arrayOfStructs (pointer, str) -> cmp1 -> cmp2 -> qsort(cmp1) ->
 -> output -> qsort(cmp2) -> output ->
 */
